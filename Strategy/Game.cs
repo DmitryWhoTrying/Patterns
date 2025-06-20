fc0ab0f858b69e7e6fa339beb8c63538d9785361
dmitry the developer <dmitryredin321@gmail.com>
2025-06-20 18:14:17 +0300

    Add strategy

---
+++ Strategy/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    internal class Game
+    {
+        int heroHealth, banditHealth;
+        Strategy? strategy;
+
+        public Game(int heroHealth, int banditHealth)
+        {
+            this.heroHealth = heroHealth;
+            this.banditHealth = banditHealth;
+        }
+
+        public void makeDecision()
+        {
+            if (heroHealth > banditHealth)
+                strategy = new Attack();
+            else if (heroHealth == banditHealth)
+                strategy = new Trade();
+            else
+                strategy = new Run();
+
+            strategy.Decision();
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(100, 100);
+
+            game.makeDecision();
+        }
+    }
+}
